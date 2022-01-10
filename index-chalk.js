0170525d14252cd7780f84dc07ecf54197195fee
vishnupaulsoni <97210258+visshnupaulsoni@users.noreply.github.com>
2022-01-10 20:07:16 +0530

    Add files via upload
    first commit

---
+++ index-chalk.js
@@ -0,0 +1,13 @@
+//ES2015 import  syntax
+import lodash from 'lodash'
+
+//const lodash = require('lodash');
+
+//const{ divide} =require('lodash');
+
+//ES2015 import
+import chalk from 'chalk';
+
+console.log(lodash.divide(20,3));
+//console.log(divide(20,3));
+console.log(chalk.red('File does not exist'));  
---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+const lodash = require('lodash');
+//or
+const{ divide} =require('lodash');
+
+//ES2015 import
+
+console.log(lodash.divide(20,3));
+console.log(divide(20,3));
